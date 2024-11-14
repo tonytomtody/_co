6434ab81c35111d909aecf48ea9cd8fce8df8c05
tonytomtody <tonytomtody@gmail.com>
2024-11-14 10:20:25 +0800

    fill

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,22 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@2
+M=0
+@1
+D=M
+@17
+D;JLE
+@2
+D=M
+@0
+D=D+M
+@2
+M=D
+@1
+D=M
+M=D-1
+@2
+0;JMP
+@17
+0;JMP
